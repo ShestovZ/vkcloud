ea6071c3929c4cb6c3094d02767b6ca6eb38be5c
ShestovZ <165843737+ShestovZ@users.noreply.github.com>
2024-11-06 22:32:09 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,72 +1,50 @@
-resource "vkcs_compute_instance" "first"{
-        name="nginx"
-        availability_zone=var.availability_zone
-        flavor_name=var.compute_flavor
-        tags=[var.tag]
-        key_pair=var.ssh_keys
-        block_device{
-                source_type="image"
-                uuid             = var.os_image
-                destination_type = "volume"
-                volume_size      = 30
-                volume_type      = "high-iops"
-                delete_on_termination = true
-        }
-        network{
-                port=vkcs_networking_port.port_nginx_in.id
-        }
-        depends_on=[vkcs_networking_router.router]
-}
-resource "vkcs_compute_instance" "second"{
-        name="mysql"
-        availability_zone=var.availability_zone2
-        flavor_name=var.compute_flavor
-        tags=[var.tag]
-        key_pair=var.ssh_keys
-        block_device{
-                source_type="image"
-                uuid=var.os_image
-                destination_type="volume"
-                volume_size=30
-                volume_type="high-iops"
-                delete_on_termination=true
-        }
-        network{
-                port=vkcs_networking_port.port_mysql_in.id
-        }
-        depends_on=[vkcs_networking_router.router]
+variable "" {
+  default = "var_server"
 }
 
-resource "vkcs_compute_instance" "third"{
-        name="services"
-        availability_zone=var.availability_zone
-        flavor_name=var.compute_flavor
-        tags=[var.tag]
-        key_pair=var.ssh_keys
-        block_device{
-                source_type="image"
-                uuid=var.os_image
-                destination_type="volume"
-                volume_size=30
-                volume_type="high-iops"
-                delete_on_termination=true
-        }
-        network{
-                port=vkcs_networking_port.port_grafana_in.id
-        }
-        depends_on=[vkcs_networking_router.router]
+resource "openstack_networking_network_v2" "network" {
+  name = "ITHUBterraformnetwork-${var.lastname}"
 }
- 
-#resource "vkcs_compute_interface_attach" "port_1VM" {
-#       instance_id = vkcs_compute_instance.first.id
-#       port_id     = vkcs_networking_port.port_nginx_in.id
-#}
-#resource "vkcs_compute_interface_attach" "port_2VM" {
-#       instance_id=vkcs_compute_instance.second.id
-#       port_id=vkcs_networking_port.port_mysql_in.id
-#}
 
-#resource "vkcs_compute_interface_attach" "port_3VM" {
-#       instance_id=vkcs_compute_instance.third.id
-#       port_id=vkcs_networking_port.port_grafana_in.id
-#}
+resource "openstack_networking_subnet_v2" "subnet" {
+  name       = "ITHUBterraformsubnet-${var.lastname}"
+  network_id = openstack_networking_network_v2.network.id
+  cidr       = "192.168.254.0/24"
+  ip_version = 4
+}
+
+resource "openstack_compute_instance_v2" "vm1" {
+  name            = "ITHUBterraforubuntu1-${var.lastname}"
+  image_id       = "Ubuntu-20.04"
+  flavor_id      = "high-iops"
+  key_pair        = "my-key"
+  security_groups = ["my-sec-group"]
+
+  network {
+    uuid = openstack_networking_network_v2.network.id
+    fixed_ip = "192.168.254.100"
+  }
+}
+
+resource "openstack_compute_instance_v2" "vm2" {
+  name            = "ITHUBterraforubuntu2-${var.lastname}"
+  image_id       = "Ubuntu-20.04"
+  flavor_id      = "high-iops"
+  key_pair        = "my-key"
+  security_groups = ["my-sec-group"]
+
+  network {
+    uuid = openstack_networking_network_v2.network.id
+    fixed_ip = "192.168.254.200"
+  }
+}
+
+resource "openstack_networking_router_v2" "router" {
+  name           = "ITHUBterraformrouter-${var.lastname}"
+  external_network_id = "public"
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface" {
+  router_id = openstack_networking_router_v2.router.id
+  subnet_id = openstack_networking_subnet_v2.subnet.id
+}
