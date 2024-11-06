3edb84343a38d927e35db96c417c71045450db32
ShestovZ <165843737+ShestovZ@users.noreply.github.com>
2024-11-06 22:46:11 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,50 +1,50 @@
-variable "" {
-  default = "var_server"
-}
+variable "instances" {
+  type = map(object({
+    name              = string
+    availability_zone = string
+    port_id           = string
+  }))
 
-resource "openstack_networking_network_v2" "network" {
-  name = "ITHUBterraformnetwork-${var.lastname}"
-}
-
-resource "openstack_networking_subnet_v2" "subnet" {
-  name       = "ITHUBterraformsubnet-${var.lastname}"
-  network_id = openstack_networking_network_v2.network.id
-  cidr       = "192.168.254.0/24"
-  ip_version = 4
-}
-
-resource "openstack_compute_instance_v2" "vm1" {
-  name            = "ITHUBterraforubuntu1-${var.lastname}"
-  image_id       = "Ubuntu-20.04"
-  flavor_id      = "high-iops"
-  key_pair        = "my-key"
-  security_groups = ["my-sec-group"]
-
-  network {
-    uuid = openstack_networking_network_v2.network.id
-    fixed_ip = "192.168.254.100"
+  default = {
+    nginx = {
+      name              = "nginx"
+      availability_zone = var.availability_zone
+      port_id           = vkcs_networking_port.port_nginx_in.id
+    },
+    mysql = {
+      name              = "mysql"
+      availability_zone = var.availability_zone2
+      port_id           = vkcs_networking_port.port_mysql_in.id
+    },
+    services = {
+      name              = "services"
+      availability_zone = var.availability_zone
+      port_id           = vkcs_networking_port.port_grafana_in.id
+    }
   }
 }
 
-resource "openstack_compute_instance_v2" "vm2" {
-  name            = "ITHUBterraforubuntu2-${var.lastname}"
-  image_id       = "Ubuntu-20.04"
-  flavor_id      = "high-iops"
-  key_pair        = "my-key"
-  security_groups = ["my-sec-group"]
+resource "vkcs_compute_instance" "instances" {
+  for_each = var.instances
+
+  name                = each.value.name
+  availability_zone   = each.value.availability_zone
+  flavor_name         = var.compute_flavor
+  tags                = [var.tag]
+  key_pair            = var.ssh_keys
+
+  block_device {
+    source_type            = "image"
+    uuid                   = var.os_image
+    destination_type       = "volume"
+    volume_size            = 30
+    volume_type            = "high-iops"
+    delete_on_termination  = true
+  }
 
   network {
-    uuid = openstack_networking_network_v2.network.id
-    fixed_ip = "192.168.254.200"
+    port = each.value.port_id
   }
-}
 
-resource "openstack_networking_router_v2" "router" {
-  name           = "ITHUBterraformrouter-${var.lastname}"
-  external_network_id = "public"
-}
-
-resource "openstack_networking_router_interface_v2" "router_interface" {
-  router_id = openstack_networking_router_v2.router.id
-  subnet_id = openstack_networking_subnet_v2.subnet.id
+  depends_on = [vkcs_networking_router.router]
 }
